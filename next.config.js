facc220a1afe712a2356ce843a1fcc6e921bec6d
rafliputraa <rafli.putra712@gmail.com>
2023-07-23 23:52:52 +0700

    add the final code

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['lh3.googleusercontent.com']
-    }
-}
+  images: {
+    domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "task.com"],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["cloudinary", "graphql-request"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
